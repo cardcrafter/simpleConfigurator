415fd84d1f522817c7ea93fb40f362b0074c0668
pluto <valcano103@gmail.com>
2025-11-18 01:33:39 +0800

    chore: remove metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,3 @@
-import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
@@ -12,10 +11,6 @@ const geistMono = Geist_Mono({
   subsets: ["latin"],
 });
 
-export const metadata: Metadata = {
-  title: "TestVM",
-  description: "Developed by Dahlquist",
-};
 
 export default function RootLayout({
   children,
